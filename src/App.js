2dd5b9b4b0a6c2e193dab92aecfd2d0deaa6a9ae
Valentin <valentin.hetriuc@gmail.com>
2025-05-14 15:53:40 +0300

    react props with createElement

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+const Pizza = (props) => {
+    return React.createElement("div", {}, 
+       [ React.createElement("h1", {}, props.name),
+        React.createElement("p", {}, props.description),
+        ],
+    );
+}
+
 const App = () => {
     return React.createElement("div", {}, 
         React.createElement("div", {},  
-        React.createElement("h1", {}, "Restaurant")),
+       [ React.createElement("h1", {}, "Restaurant"),
+        React.createElement(Pizza, {name: "Pepperoni", description: "Mozzarella Cheese, Pepperoni"}),
+        React.createElement(Pizza, {name: "Cheese", description: "Mozzarella Cheese"}),
+        React.createElement(Pizza, {name: "Pepperoni", description: "Mozzarella Cheese, Pepperoni"}),
+       ]),
      );
 }
 
