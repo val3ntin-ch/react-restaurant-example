49fb40769d4b9192f0c925db6234c235c9990bc5
Valentin <valentin.hetriuc@gmail.com>
2025-05-28 18:05:10 +0300

    add server amd  connection to components

--- src/Pizza.jsx
+++ src/Pizza.jsx
@@ -1,8 +1,9 @@
-export default function Pizza({name, description}) {
+export default function Pizza({ name, description, image }) {
   return (
-    <div className='pizza'>
-        <h1>{name}</h1>
-        <p>{description}</p>
+    <div className="pizza">
+      <h1>{name}</h1>
+      <p>{description}</p>
+      {image && <img src={image} alt={name} />}
     </div>
-  )
+  );
 }
